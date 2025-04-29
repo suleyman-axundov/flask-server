70c31defd86992bf04b86129cb64d55e1d958a31
Suleyman <101743693+suleyman-axundov@users.noreply.github.com>
2025-04-30 00:14:20 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, request
 from flask_cors import CORS
+from flask_talisman import Talisman  # Əlavə etdik
 import smtplib
 from email.mime.text import MIMEText
 import os
 
 app = Flask(__name__)
 CORS(app)
+Talisman(app)  # HTTPS yönləndirmə və təhlükəsizlik başlıqları
 
 # Gmail hesab məlumatları
-GMAIL_USER = 'suleyman.axundov2004@gmail.com'  # <-- Buraya öz gmail ünvanını yaz
-GMAIL_PASSWORD = 'ldvdfdkbubkffzty'   # <-- App Password-u boşluqsuz yazdım
+GMAIL_USER = 'suleyman.axundov2004@gmail.com'
+GMAIL_PASSWORD = 'ldvdfdkbubkffzty'
 
 @app.route('/')
 def home():
@@ -21,16 +23,13 @@ def submit():
     telefon = request.form.get('telefon')
     mesaj = request.form.get('mesaj')
 
-    # E-mail məzmunu
     email_content = f"Yeni müraciət:\n\nAd: {ad}\nTelefon: {telefon}\nMesaj: {mesaj}"
 
-    # E-mail başlıqları
     msg = MIMEText(email_content)
     msg['Subject'] = 'Saytdan Yeni Müraciət'
     msg['From'] = GMAIL_USER
-    msg['To'] = GMAIL_USER  # Özünə göndərmək üçün
+    msg['To'] = GMAIL_USER
 
-    # SMTP server ilə göndərmək
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
         server.login(GMAIL_USER, GMAIL_PASSWORD)
         server.send_message(msg)
